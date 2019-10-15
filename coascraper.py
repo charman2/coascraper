5ece9c11269c98c9a65888ffd2853d360c2c62b4
Ciaran Harman <Ciaran.harman@gmail.com>
2019-10-15 18:49:27 -0400

    cleaned up search

--- coascraper.py
+++ coascraper.py
@@ -32,7 +32,7 @@ except Scopus429Error:
     Documents = AuthorRetrieval(Author_id).get_documents()
 
 # How many googled pages to scrape looking for email addresses
-num_url_search_email = 10
+num_url_search_email = 5
 
 # today's date
 today = datetime.datetime.now()
@@ -61,7 +61,7 @@ for doc in Documents:
 
                 # get an affiliation with an OrgID (starts with a 6)
                 affil_id = CoAuthor.affiliation_current
-                if affil_id[0] != '6':
+                if affil_id[0] != '6' and CoAuthor.affiliation_history is not None:
                     affil_id_list = [a for a in CoAuthor.affiliation_history if a[0] == '6']
                     if len(affil_id_list) > 0:
                         affil_id = affil_id_list[0]
@@ -70,20 +70,28 @@ for doc in Documents:
                 print(f'affiliation: {affil_name}')
 
                 # google them
-                google_results = search(' '.join([CoAuthor.given_name, CoAuthor.surname, affiliation.org_domain]), num=10)
+                google_results = search(' '.join(['email', CoAuthor.given_name, CoAuthor.surname, affiliation.org_domain]),
+                                        num=num_url_search_email, only_standard=True, pause=5.)
                 # scrape the results, looking for their email address
                 email_list = []
-                for tryurl in range(num_url_search_email):
-                    url = next(google_results)
-                    html = requests.get(url).text
-                    print(f'  scraping {url}', len(html))
-                    # this uses regex to find something like an email address
-                    email_list += re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", html)
-                # keep only those that contain the first three letters of the author's lastname
-                email = ', '.join(
-                    list(set([e for e in email_list if CoAuthor.surname[:3].lower() in e.split('@')[0].lower()])))
-                # if that is removing too many results, comment that line out and uncomment this one
-                # email = ', '.join(list(set([e for e in email_list])))
+                for url in google_results:
+                    if not url[-3:]=='pdf':
+                        print(f'  scraping {url}')
+                        try:
+                            html = requests.get(url).text
+                            # this uses regex to find something like an email address
+                            scraped_email = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", html)
+                            email_list += scraped_email
+                            # keep only unique entries that contain the first three letters of the author's lastname
+                            email_list = list(set([e for e in email_list if CoAuthor.surname[:3].lower() in e.split('@')[0].lower()]))
+                            # if that is removing too many valid results, comment that line out and uncomment this one
+                            # email = list(set([e for e in email_list]))
+                            # stop if you have at least 2 unique emails
+                            if len(email_list)>1:
+                                break
+                        except:
+                            pass
+                email = ', '.join(email_list)
                 print(f'email:       {email}')
 
                 # add the results to the dataframe
